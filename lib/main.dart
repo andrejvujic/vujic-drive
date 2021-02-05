304c02d0acc75f2fbf0f110864b40d9b4a2d9af1
Andrej Vujic <vujicandrej365@gmail.com>
2021-02-05 16:09:36 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vujic_drive/auth.dart';
+import 'package:vujic_drive/services/app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppService(),
+      child: App(),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Auth(),
+    );
+  }
+}
