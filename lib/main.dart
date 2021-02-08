78bcef2303fdcc5c832c5cf2a338f85af79ff681
Andrej Vujic <vujicandrej365@gmail.com>
2021-02-08 12:15:38 +0100

    setup firebase analytics and performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vujic_drive/auth.dart';
 import 'package:vujic_drive/services/app.dart';
 
+FirebaseAnalytics analytics;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  analytics = FirebaseAnalytics();
   await Firebase.initializeApp();
   runApp(
     ChangeNotifierProvider(
